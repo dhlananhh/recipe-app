ec736cbe8bcf5cd7af967c99d5da73d12bc2d98f
Lan Anh <dhlananh2309@gmail.com>
2025-09-05 22:40:20 +0700

    feat: create main layout with Navbar and Footer

---
+++ src/app/(main)/layout.tsx
@@ -0,0 +1,19 @@
+import { Navbar } from "@/components/shared/Navbar";
+import { Footer } from "@/components/shared/Footer";
+
+
+export default function MainLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="relative flex min-h-screen flex-col">
+      <Navbar />
+      <main className="container mx-auto max-w-7xl pt-12 flex-grow">
+        { children }
+      </main>
+      <Footer />
+    </div>
+  );
+}
