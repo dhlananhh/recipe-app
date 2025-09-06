ff9275b163948779c949c913efe07be9e8a08695
Lan Anh <dhlananh2309@gmail.com>
2025-09-06 13:28:32 +0700

    style: code formatting

--- src/components/magicui/meteors.tsx
+++ src/components/magicui/meteors.tsx
@@ -1,7 +1,12 @@
 "use client";
 
+
 import { cn } from "@/lib/utils";
-import React, { useEffect, useState } from "react";
+import React, {
+  useEffect,
+  useState
+} from "react";
+
 
 interface MeteorsProps {
   number?: number;
@@ -13,6 +18,7 @@ interface MeteorsProps {
   className?: string;
 }
 
+
 export const Meteors = ({
   number = 20,
   minDelay = 0.2,
@@ -22,12 +28,12 @@ export const Meteors = ({
   angle = 215,
   className,
 }: MeteorsProps) => {
-  const [meteorStyles, setMeteorStyles] = useState<Array<React.CSSProperties>>(
+  const [ meteorStyles, setMeteorStyles ] = useState<Array<React.CSSProperties>>(
     [],
   );
 
   useEffect(() => {
-    const styles = [...new Array(number)].map(() => ({
+    const styles = [ ...new Array(number) ].map(() => ({
       "--angle": -angle + "deg",
       top: "-5%",
       left: `calc(0% + ${Math.floor(Math.random() * window.innerWidth)}px)`,
@@ -37,24 +43,24 @@ export const Meteors = ({
         "s",
     }));
     setMeteorStyles(styles);
-  }, [number, minDelay, maxDelay, minDuration, maxDuration, angle]);
+  }, [ number, minDelay, maxDelay, minDuration, maxDuration, angle ]);
 
   return (
     <>
-      {[...meteorStyles].map((style, idx) => (
+      { [ ...meteorStyles ].map((style, idx) => (
         // Meteor Head
         <span
-          key={idx}
-          style={{ ...style }}
-          className={cn(
+          key={ idx }
+          style={ { ...style } }
+          className={ cn(
             "pointer-events-none absolute size-0.5 rotate-[var(--angle)] animate-meteor rounded-full bg-zinc-500 shadow-[0_0_0_1px_#ffffff10]",
             className,
-          )}
+          ) }
         >
-          {/* Meteor Tail */}
+          {/* Meteor Tail */ }
           <div className="pointer-events-none absolute top-1/2 -z-10 h-px w-[50px] -translate-y-1/2 bg-gradient-to-r from-zinc-500 to-transparent" />
         </span>
-      ))}
+      )) }
     </>
   );
 };
