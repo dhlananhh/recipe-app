35ac0c8194796366b7d190e559402b0ca19f0ec0
Lan Anh <dhlananh2309@gmail.com>
2025-09-06 13:57:32 +0700

    feat: implement fetching categories and recipes by category

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,4 +1,8 @@
-import { Recipe, RecipeDetails } from "@/types";
+import {
+  Recipe,
+  RecipeDetails,
+  Category
+} from "@/types";
 
 
 const API_BASE_URL_V2 = "https://www.themealdb.com/api/json/v2/9973533";
@@ -39,6 +43,32 @@ export async function getRandomRecipes(): Promise<Recipe[]> {
     return data.meals || [];
   } catch (error) {
     console.error("Failed to get random selection of recipes:", error);
+    return [];
+  }
+}
+
+
+export async function getAllCategories(): Promise<Category[]> {
+  try {
+    const response = await fetch(`${API_BASE_URL_V1}/categories.php`);
+    if (!response.ok) throw new Error("Network response was not ok");
+    const data = await response.json();
+    return data.categories || [];
+  } catch (error) {
+    console.error("Failed to get all categories:", error);
+    return [];
+  }
+}
+
+
+export async function getRecipesByCategory(categoryName: string): Promise<Recipe[]> {
+  try {
+    const response = await fetch(`${API_BASE_URL_V1}/filter.php?c=${categoryName}`);
+    if (!response.ok) throw new Error("Network response was not ok");
+    const data = await response.json();
+    return data.meals || [];
+  } catch (error) {
+    console.error(`Failed to get recipes for category ${categoryName}:`, error);
     return [];
   }
 }
